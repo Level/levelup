7ae020e7c15f9f0e5f95460498bcfe674de4d50a
Rod Vagg <rod@vagg.org>
2012-12-16 12:47:35 +1100

    improved benchmarks

--- test/benchmarks/tests/index.js
+++ test/benchmarks/tests/index.js
@@ -4,26 +4,30 @@
 
 module.exports = {
     'put(int, string) x 1000': {
-        'LevelUP'           : require('./put_int_string_x1000_levelup')
-      , 'LevelUP (release)' : require('./put_int_string_x1000_levelup')
-      , 'Leveled'           : require('./put_int_string_x1000_leveled')
+        'LevelUP'             : require('./put_int_string_x1000_levelup')
+      , 'LevelUP (release)'   : require('./put_int_string_x1000_levelup')
+      , 'LevelUP (no Snappy)' : require('./put_int_string_x1000_levelup')
+      , 'Leveled'             : require('./put_int_string_x1000_leveled')
     }
 
   , 'put(int, string) x 100,000': {
-        'LevelUP'           : require('./put_int_string_x100000_levelup')
-      , 'LevelUP (release)' : require('./put_int_string_x100000_levelup')
-      , 'Leveled'           : require('./put_int_string_x100000_leveled')
+        'LevelUP'             : require('./put_int_string_x100000_levelup')
+      , 'LevelUP (release)'   : require('./put_int_string_x100000_levelup')
+      , 'LevelUP (no Snappy)' : require('./put_int_string_x100000_levelup')
+      , 'Leveled'             : require('./put_int_string_x100000_leveled')
     }
 
   , 'batch(int, string) x 1000': {
-        'LevelUP'           : require('./batch_int_string_x1000_levelup')
-      , 'LevelUP (release)' : require('./batch_int_string_x1000_levelup')
-      , 'Leveled'           : require('./batch_int_string_x1000_leveled')
+        'LevelUP'             : require('./batch_int_string_x1000_levelup')
+      , 'LevelUP (release)'   : require('./batch_int_string_x1000_levelup')
+      , 'LevelUP (no Snappy)' : require('./batch_int_string_x1000_levelup')
+      , 'Leveled'             : require('./batch_int_string_x1000_leveled')
     }
 
   , 'batch(int, string) x 100,000': {
-        'LevelUP'           : require('./batch_int_string_x100000_levelup')
-      , 'LevelUP (release)' : require('./batch_int_string_x100000_levelup')
-      , 'Leveled'           : require('./batch_int_string_x100000_leveled')
+        'LevelUP'             : require('./batch_int_string_x100000_levelup')
+      , 'LevelUP (release)'   : require('./batch_int_string_x100000_levelup')
+      , 'LevelUP (no Snappy)' : require('./batch_int_string_x100000_levelup')
+      , 'Leveled'             : require('./batch_int_string_x100000_leveled')
     }
 }
