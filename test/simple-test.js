a0a1d78db4d6e0c4b7e59b284defcbe535e07ecf
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-02-03 05:05:42 +0100

    InitializationError -> InitError to reduce line lengths

--- test/simple-test.js
+++ test/simple-test.js
@@ -1,6 +1,7 @@
 /* Copyright (c) 2012-2013 LevelUP contributors
  * See list at <https://github.com/rvagg/node-levelup#contributing>
- * MIT +no-false-attribs License <https://github.com/rvagg/node-levelup/blob/master/LICENSE>
+ * MIT +no-false-attribs License
+ * <https://github.com/rvagg/node-levelup/blob/master/LICENSE>
  */
 
 var buster  = require('buster')
@@ -18,7 +19,7 @@ buster.testCase('Basic API', {
   , 'levelup()': function () {
       assert.isFunction(levelup)
       assert.equals(levelup.length, 3) // location, options & callback arguments
-      assert.exception(levelup, 'InitializationError') // no location
+      assert.exception(levelup, 'InitError') // no location
     }
 
   , 'default options': function (done) {
