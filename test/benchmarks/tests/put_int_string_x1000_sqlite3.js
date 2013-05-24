a51025db1ae59bb8afd84f79a999bb74b30f2178
Ricardo Tomasi <ricardobeat@gmail.com>
2013-05-24 06:18:05 -0300

    Optimize SQLite PUT benchmark

--- test/benchmarks/tests/put_int_string_x1000_sqlite3.js
+++ test/benchmarks/tests/put_int_string_x1000_sqlite3.js
@@ -8,14 +8,22 @@ var fn = function (puts, db, cb) {
   if (this.cycle == null) this.cycle = 0
   else this.cycle++
 
-  for (var i = 0; i < puts; i++)
-    db.exec(
-        'INSERT INTO bench VALUES('
-      + String(this.cycle * puts + i)
-      + ', "It\'ll be top end no worries stands out like a bushie. It\'ll be cream no dramas flat out like a rotten. As busy as a slabs bloody built like a stonkered. Get a dog up ya oldies no dramas lets get some bottle-o. Built like a schooner as busy as a big smoke. You little ripper ute my you little ripper dag."'
-      + ')'
-      , after
-    )
+  var self = this
+
+  db.serialize(function(){
+    db.run('BEGIN')
+    db.parallelize(function(){
+      var s = db.prepare('INSERT INTO bench VALUES(?, ?)')
+      for (var i = 0; i < puts; i++)
+        s.run(
+          String(self.cycle * puts + i),
+          "It\'ll be top end no worries stands out like a bushie. It\'ll be cream no dramas flat out like a rotten. As busy as a slabs bloody built like a stonkered. Get a dog up ya oldies no dramas lets get some bottle-o. Built like a schooner as busy as a big smoke. You little ripper ute my you little ripper dag."
+        , after)
+
+      s.finalize()
+    })
+    db.run('COMMIT')
+  })
 }
 
 module.exports = fn.bind(null, 1000)
