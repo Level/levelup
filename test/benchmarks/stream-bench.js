0fe16ba0552be36ae0f8fa382ccc747345426c18
Rod Vagg <rod@vagg.org>
2013-07-21 21:51:31 +1000

    added stream-bench.js from write-stream-optimisation

---
+++ test/benchmarks/stream-bench.js
@@ -0,0 +1,43 @@
+const levelup = require('../../')
+    , crypto  = require('crypto')
+    , srcdb   = levelup('/tmp/source.db')
+    , dstdb   = levelup('/tmp/destination.db')
+
+    , batch   = 10000
+    , total   = 200000
+ 
+function fillBatch (start, callback) {
+  var b = []
+  for (var i = start; i < start + batch; i++) {
+    b.push({ type: 'put', key: i, value: crypto.randomBytes(100) })
+  }
+  srcdb.batch(b, callback)
+}
+
+function populate (start, callback) {
+  if (start > total)
+    return callback()
+  fillBatch(start, function (err) {
+    if (err) throw err
+    populate(start + batch, callback)
+  })
+}
+
+srcdb.on('ready', function () {
+  var start = Date.now()
+
+  populate(0, function () {
+    var batchTime = Date.now() - start
+
+    console.log('--------------------------------------------------------------')
+    console.log('Filled source! Took', batchTime + 'ms, streaming to destination...')
+
+    start = Date.now()
+    srcdb.createReadStream()
+      .on('end', function () {
+        var copyTime = Date.now() - start
+        console.log('Done! Took', copyTime + 'ms,', Math.round((batchTime / copyTime) * 100) + '% of batch time')
+      })
+      .pipe(dstdb.createWriteStream())
+  })
+})
