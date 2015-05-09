261794a2fbcbd03c0e0075f5fc5d6c44b1f7fb86
Braydon Fuller <braydon@bitpay.com>
2015-05-09 10:42:20 -0400

    _active to _activeOperations, _getCallback to guardCallback

--- test/close-racecondition-test.js
+++ test/close-racecondition-test.js
@@ -16,25 +16,25 @@ buster.testCase('Close', {
   , 'tearDown': common.commonTearDown
   , 'several put() operations followed by a close()': function(done) {
     this.openTestDatabase(function(db) {
-      var putsCompleted = 0;
+      var putsCompleted = 0
       db.put('some key 1', 'some value 1 stored in the database', function (err) {
-        putsCompleted++;
-        refute(err);
-      });
+        putsCompleted++
+        refute(err)
+      })
       db.put('some key 2', 'some value 2 stored in the database', function (err) {
-        putsCompleted++;
-        refute(err);
-      });
+        putsCompleted++
+        refute(err)
+      })
       db.put('some key 3', 'some value 3 stored in the database', function (err) {
-        putsCompleted++;
-        refute(err);
-      });
+        putsCompleted++
+        refute(err)
+      })
       db.close(function(err) {
-        assert(db._active === 0, 'close called while there are still active operations');
-        assert(putsCompleted === 3, 'close completed before put operations');
-        refute(err);
-        done();
-      });
+        assert(db._activeOperations === 0, 'close called while there are still active operations')
+        assert(putsCompleted === 3, 'close completed before put operations')
+        refute(err)
+        done()
+      })
     }.bind(this))
   }
-});
+})
