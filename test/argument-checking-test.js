f1197685acb96aa69ba425284dc81e4289b7fdf3
Julian Gruber <julian@juliangruber.com>
2015-05-26 18:28:21 +0200

    remove .approximateSize()

--- test/argument-checking-test.js
+++ test/argument-checking-test.js
@@ -64,37 +64,6 @@ buster.testCase('Argument checking', {
       })
     }
 
-  , 'test approximateSize() throwables': function (done) {
-      this.openTestDatabase(function (db) {
-
-        assert.exception(
-            db.approximateSize.bind(db)
-          , { name: 'ReadError', message: 'approximateSize() requires start, end and callback arguments' }
-          , 'no-arg approximateSize() throws'
-        )
-
-        assert.exception(
-            db.approximateSize.bind(db, 'foo')
-          , { name: 'ReadError', message: 'approximateSize() requires start, end and callback arguments' }
-          , 'callback-less, 1-arg approximateSize() throws'
-        )
-
-        assert.exception(
-            db.approximateSize.bind(db, 'foo', 'bar')
-          , { name: 'ReadError', message: 'approximateSize() requires start, end and callback arguments' }
-          , 'callback-less, 2-arg approximateSize() throws'
-        )
-
-        assert.exception(
-            db.approximateSize.bind(db, 'foo', 'bar', {})
-          , { name: 'ReadError', message: 'approximateSize() requires start, end and callback arguments' }
-          , 'callback-less, 3-arg approximateSize(), no cb throws'
-        )
-
-        done()
-      })
-    }
-
   , 'test batch() throwables': function (done) {
       this.openTestDatabase(function (db) {
 
