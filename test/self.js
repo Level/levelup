8d123e93484199092e6a9ac083f50b689cf54685
Vincent Weevers <mail@vincentweevers.nl>
2020-11-25 14:51:50 +0100

    Temporarily reduce amount of tests

--- test/self.js
+++ test/self.js
@@ -12,17 +12,17 @@ var levelup = require('../lib/levelup')
 var suite = require('.')
 var noop = function () {}
 
-suite({
-  test: test,
-  factory: function (options) {
-    return levelup(encode(memdown(), options))
-  },
-  clear: true,
-  deferredOpen: true,
-  promises: true,
-  streams: true,
-  encodings: true
-})
+// suite({
+//   test: test,
+//   factory: function (options) {
+//     return levelup(encode(memdown(), options))
+//   },
+//   clear: true,
+//   deferredOpen: true,
+//   promises: true,
+//   streams: true,
+//   encodings: true
+// })
 
 suite({
   test: test,
@@ -32,7 +32,7 @@ suite({
   clear: true,
   deferredOpen: true,
   promises: true,
-  streams: true,
+  streams: false,
   encodings: false
 })
 
@@ -40,21 +40,21 @@ suite({
 // - Have abstract-leveldown use level-errors
 // - Perform type checks in same order (e.g. check key before callback)
 // - Add db.isClosed(), isOpen() to abstract-leveldown
-suite({
-  test: noop,
-  factory: function (options) {
-    return memdown()
-  },
-  clear: true,
-  deferredOpen: false,
-  promises: false,
-  streams: false,
-  encodings: false
-})
+// suite({
+//   test: noop,
+//   factory: function (options) {
+//     return memdown()
+//   },
+//   clear: true,
+//   deferredOpen: false,
+//   promises: false,
+//   streams: false,
+//   encodings: false
+// })
 
 // Integration tests that can't use a generic testCommon.factory()
-require('./self/manifest-test')
-
-if (!process.browser) {
-  require('./browserify-test')(test)
-}
+// require('./self/manifest-test')
+//
+// if (!process.browser) {
+//   require('./browserify-test')(test)
+// }
