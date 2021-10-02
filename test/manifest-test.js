e2e2c341ac7c29e5183bea299d9d7b2b329046e0
Vincent Weevers <mail@vincentweevers.nl>
2021-10-02 11:34:58 +0200

    Set `supports.status` to true

--- test/manifest-test.js
+++ test/manifest-test.js
@@ -3,11 +3,11 @@ const suite = require('level-supports/test')
 module.exports = function (test, testCommon) {
   suite(test, testCommon)
 
-  // TODO (once manifest lands in other modules): add integration tests.
+  // TODO: add integration tests.
   test('manifest has expected properties', function (t) {
     const db = testCommon.factory()
 
-    t.is(db.supports.status, false)
+    t.is(db.supports.status, true)
     t.is(db.supports.deferredOpen, true)
     t.is(db.supports.openCallback, true)
     t.is(db.supports.promises, true)
